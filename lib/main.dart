db75cbdb3741b776ca248f2446076f8215110bb2
N1-San <33519431+N1-San@users.noreply.github.com>
2025-05-19 18:18:03 +0530

    tabs and favorite screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
-import 'package:meals_app_flutter/screens/categories.dart';
+// import 'package:meals_app_flutter/screens/categories.dart';
+import 'package:meals_app_flutter/screens/tabs.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -24,7 +25,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen()
+      home: const TabsScreen()
     );
   }
 }
