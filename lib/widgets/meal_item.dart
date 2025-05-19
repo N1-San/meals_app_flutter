3043b840648b439b8b2422d09e6f5abbd648e53e
N1-San <33519431+N1-San@users.noreply.github.com>
2025-05-19 16:12:53 +0530

    meal details screen basic

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -6,9 +6,14 @@ import 'package:meals_app_flutter/models/meal.dart';
 import 'package:meals_app_flutter/widgets/meal_item_trait.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({
+    super.key, 
+    required this.meal,
+    required this.onSelectMeal,
+  });
 
   final Meal meal;
+  final void Function(Meal meal) onSelectMeal;
 
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() + 
@@ -28,7 +33,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          onSelectMeal(meal);
+        },
         child: Stack(
           children: [
             FadeInImage(
